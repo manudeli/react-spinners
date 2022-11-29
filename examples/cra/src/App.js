4f8a85ca347b106c46c9947346b9b0d347fe2ddf
Jonghyeon Ko <manudeli.ko@gmail.com>
2022-11-29 19:39:31 +0900

    add cra, ssr, cdn examples

---
+++ examples/cra/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spinner from 'react-spinners'
+
+function App () {
+  return (
+    <div className="App">
+      <Spinner/>
+    </div>
+  )
+}
+
+export default App
---
+++ examples/ssr/index.js
@@ -0,0 +1,29 @@
+const { createElement } = require('react')
+const express = require('express')
+const { renderToString } = require('react-dom/server')
+const Spinner = require('react-spinners')
+
+const port = 3000
+const app = express()
+
+app.get('*', (req, res) => {
+  const el = createElement(Spinner)
+  const html = renderToString(el)
+
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        ${html}
+    </body>
+    </html>
+  `)
+})
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
